7a7e7b1e20273b5080f61b3c90638f99fd048aa8
Yousef Alsaadany <58972656+y0usefalsaadany@users.noreply.github.com>
2022-06-27 13:01:40 +0200

    Update VisitController.php

--- Controllers/VisitController.php
+++ Controllers/VisitController.php
@@ -4,11 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Visit;
+use Illuminate\Support\Facades\Http;
+
 class VisitController extends Controller
 {
     function countVisit(Request $request){
+        $api = Http::get('http://ipwho.is/'. $request->ip());  
+        $data = json_decode($api, true); 
         $visitTable =  new Visit;
         $visitTable->ip = $request->ip();
+        $visitTable->city = $data['city'];
         $visitTable->save();
+        $countVisits = count(Visit::all());
+        return $countVisits;
     }
 }
