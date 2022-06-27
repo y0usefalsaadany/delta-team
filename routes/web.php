291bdbed0a36e6d5426ccfed371b3c8f8e970ef8
Yousef Alsaadany <58972656+y0usefalsaadany@users.noreply.github.com>
2022-06-27 12:00:07 +0200

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/mail',[App\Http\Controllers\MailController::class,'index']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/test', [App\Http\Controllers\TestController::class, 'handle'])->middleware("throttle:visits");
+Route::get('/visit', [App\Http\Controllers\VisitController::class, 'countVisit']);
