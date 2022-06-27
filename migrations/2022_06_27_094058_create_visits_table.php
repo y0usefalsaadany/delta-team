6ca6e3dce8eae3a54c52ef1397c9c2eb6923f976
Yousef Alsaadany <58972656+y0usefalsaadany@users.noreply.github.com>
2022-06-27 13:02:06 +0200

    Update 2022_06_27_094058_create_visits_table.php

--- migrations/2022_06_27_094058_create_visits_table.php
+++ migrations/2022_06_27_094058_create_visits_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('visits', function (Blueprint $table) {
             $table->id();
             $table->ipAddress('ip');
+            $table->string('city');
             $table->timestamps();
         });
     }
